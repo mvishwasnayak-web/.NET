6d23081100be4b4db2ffb95c0c6136ac4d662360
mvishwasnayak-web <mvishwasnayak@gmail.com>
2026-01-29 11:47:23 +0530

    Add files via upload

---
+++ StudentWebAPI/StudentWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using StudentWebAPI.Model;
+
+namespace StudentWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Appsettings _settings;
+
+        public HomeController(IOptions<Appsettings>settings)
+        {
+            _settings = settings.Value;
+
+        }
+        public IActionResult Index()
+        {
+            return Content(
+                $"Name of the Application: {_settings.ApplicationName} \n "+
+                $"Support: {_settings.SupportEmail}");
+                }
+        }
+    }
+
---
+++ StudentWebAPI/StudentWebAPI/Model/Appsettings.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentWebAPI.Model
+{
+    public class Appsettings
+    {
+        public string? ApplicationName { get; set; }
+
+        public string? SupportEmail { get; set; }
+    }
+}
